c905e01c6bca608d974b9a6bf296368aaa05a533
SizNi <wwwwwwwww@list.ru>
2022-12-25 16:45:33 +0300

    load dotenv fix

--- Makefile
+++ Makefile
@@ -15,3 +15,6 @@ pytest:
 PORT ?= 8000
 start:
 	poetry run gunicorn -w 5 -b 0.0.0.0:$(PORT) page_analyzer:app
+
+server:
+	sudo service postgresql start
--- page_analyzer/connection.py
+++ page_analyzer/connection.py
@@ -2,9 +2,9 @@ import psycopg2
 import os
 from dotenv import load_dotenv
 
-dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
-if os.path.exists(dotenv_path):
-    load_dotenv(dotenv_path)
+#dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+#if os.path.exists(dotenv_path):
+load_dotenv()
 
 
 def connect_db_2():
